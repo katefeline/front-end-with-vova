e48395f1b1f5f167cbcee71aadb082a010f8f391
Kateryna Shadrina <katefeline@gmail.com>
2022-12-16 12:27:08 +0200

    added function to make math operations

--- lesson01/task4/script.js
+++ lesson01/task4/script.js
@@ -1,25 +1,35 @@
-let question = prompt('What math operation do you want to do? There are 4 options: add, sub, mult, div', '');
+// let question = prompt('What math operation do you want to do? There are 4 options: add, sub, mult, div', '');
+// let firstInput = +prompt('Enter first value', '');
+// let secondInput = +prompt('Enter second value', '');
+//
+// switch (question) {
+//     case 'add': {
+//         console.log(firstInput + secondInput);
+//         break;
+//     }
+//     case 'sub': {
+//         console.log(firstInput - secondInput);
+//         break;
+//     }
+//     case 'mult': {
+//         console.log(firstInput * secondInput);
+//         break;
+//     }
+//     case 'div': {
+//         console.log(firstInput / secondInput);
+//         break;
+//     }
+//     default: {
+//         console.log('Oops, I don`t know this operation. You can try again.');
+//     }
+// }
+
+let question = prompt('What math operation do you want to do? There are 4 options: +, -, *, /', '');
 let firstInput = +prompt('Enter first value', '');
 let secondInput = +prompt('Enter second value', '');
 
-switch (question) {
-    case 'add': {
-        console.log(firstInput + secondInput);
-        break;
-    }
-    case 'sub': {
-        console.log(firstInput - secondInput);
-        break;
-    }
-    case 'mult': {
-        console.log(firstInput * secondInput);
-        break;
-    }
-    case 'div': {
-        console.log(firstInput / secondInput);
-        break;
-    }
-    default: {
-        console.log('Oops, I don`t know this operation. You can try again.');
-    }
+let mathFunction = function () {
+    return eval(`${firstInput} ${question} ${secondInput}`);
 }
+
+console.log(mathFunction());
