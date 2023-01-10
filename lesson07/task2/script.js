4b0b23dd48502eae774e7f5a62f692788b638222
Kateryna Shadrina <katefeline@gmail.com>
2023-01-10 20:04:43 +0200

    redone with improvement of code

--- lesson07/task2/script.js
+++ lesson07/task2/script.js
@@ -1,28 +1,32 @@
 'use strict';
 
 let padString = function (str, length, symbol, boolean = true) {
-
-
-    if (str.trim() === '' || str === null || isNaN(str)) {
-        console.log('Symbol cannot be more than 1 char');
-    } else {
-        if (str.length >= length) {
+    if (typeof str !== 'string') {
+        return 'Error of string';
+    }
+    if (typeof length !== 'number') {
+        return 'Error of length';
+    }
+    if (symbol.trim().length > 1 || typeof symbol !== 'string') {
+        return 'Error of symbol';
+    }
+    if (str.length >= length) {
             return str.substring(0,length);
+    } else {
+        let num = length - str.length;
+        let symbol1 = symbol;
+        for(let i = 0;i<num-1;i++) {
+            symbol += symbol1;
+        }
+        if (boolean === false) {
+            return symbol + str;
         } else {
-            let num = length - str.length;
-            let symbol1 = symbol;
-            for(let i = 0;i<num-1;i++) {
-                symbol += symbol1;
-            }
-            if (boolean === false) {
-                return symbol + str;
-            } else {
-                return str + symbol;
-            }
+            return str + symbol;
         }
     }
-
 }
+
+
 
 console.log(padString('boom',19, '+'));
 console.log(padString('wow', 11, '$', false));
